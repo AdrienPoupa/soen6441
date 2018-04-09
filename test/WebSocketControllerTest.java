dfa27d422e0de053b4d75b5aa683465070c24ef8
AdrienPoupa <adrien@poupa.fr>
2018-04-09 18:43:26 -0400

    Also test port 9000

--- test/WebSocketControllerTest.java
+++ test/WebSocketControllerTest.java
@@ -75,4 +75,32 @@ public class WebSocketControllerTest {
             }
         });
     }
+
+    /**
+     * Test for the accept WebSocket
+     */
+    @Test
+    public void testAcceptWebSocketOtherPort() {
+        TestServer server = testServer(9000);
+        running(server, () -> {
+            try {
+                AsyncHttpClientConfig config = new DefaultAsyncHttpClientConfig.Builder().setMaxRequestRetry(0).build();
+                AsyncHttpClient client = new DefaultAsyncHttpClient(config);
+                WebSocketClient webSocketClient = new WebSocketClient(client);
+
+                try {
+                    String serverURL = "ws://localhost:9000/ws";
+                    WebSocketClient.LoggingListener listener = new WebSocketClient.LoggingListener(message -> {});
+                    CompletableFuture<WebSocket> completionStage = webSocketClient.call(serverURL, listener);
+                    await().until(completionStage::isDone);
+                    assertThat(completionStage)
+                            .hasNotFailed();
+                } finally {
+                    client.close();
+                }
+            } catch (Exception e) {
+                fail("Unexpected exception", e);
+            }
+        });
+    }
 }
